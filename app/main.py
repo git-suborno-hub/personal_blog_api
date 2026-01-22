5899830e80cd9ab86587783bbf6bd36907edafe9
git-suborno-hub <suborno709@gmail.com>
2026-01-22 19:20:36 +0600

    Start the clean project again

--- app/main.py
+++ app/main.py
@@ -1,29 +1,9 @@
-from fastapi import FastAPI, Depends
-from .database import get_db
-from sqlalchemy.orm import Session
-from . import models
-from .database import engine
-from .routers import articles
+from fastapi import FastAPI
 
-app=FastAPI(
-    title="personal blog api",
-    description = "A simple blog backend build with FastAPI",
-    version= "0.1.0"
-)
+app=FastAPI()
 
 @app.get("/")
-def root():
-    return {"message":"welcome to personal blog api"}
 
-@app.get("/test-db")
-def test_db(db: Session = Depends(get_db)):
-    return {"message": "Database connection successfully!"}
-
-models.Base.metadata.create_all(bind=engine)
-
-# Health check endpoint
 @app.get("/health")
 def health_check():
-    return {"status": "ok"}
-
-app.include_router(articles.router)
+    return {"status":"ok"}
